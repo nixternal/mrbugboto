773b1372b1acba7f2b85b78f16206e55e82b55ad
nixternal <nixternal@6cc2b892-b2fc-11de-a2d3-9dcf383ffee4>
2009-10-08 21:10:22 +0000

    * Added ability for Bugzilla trackers - tested kde and gnome  * fixed issue with AttributeError in mrbugboto.py  * Updated index.py and style.css

--- trunk/mrbugboto.py
+++ trunk/mrbugboto.py
@@ -10,14 +10,14 @@ import re
 
 def OnBlipSubmitted(properties, context):
     blip = context.GetBlipById(properties['blipId'])
-    bliptext = blip.GetDocument().GetText()
-    m = re.match('\!(.*)\#(.*)', bliptext)
-    bug = runTracker(m.group(1), m.group(2))
-    #sub_blip = blip.CreateChild()
+    m = re.match('!(.+)#(.+)', blip.GetDocument().GetText())
+    try:
+	bug = runTracker(m.group(1), m.group(2))
+    except AttributeError:
+	return
     builder = OpBuilder(context)
     blip.GetDocument().SetText(" ")
     builder.DocumentAppendMarkup(blip.waveId, blip.waveletId, properties['blipId'], bug)
-
 
 def OnRobotAdded(properties, context):
     context.GetRootWavelet().CreateBlip().GetDocument().SetText('Domo arigato, Mr. Bugboto')
@@ -26,7 +26,7 @@ if __name__ == '__main__':
     myRobot = robot.Robot('mrbugboto',
 	    image_url='http://www.mrbugboto.appspot.com/assets/icon.png',
 	    version='1',
-	    profile_url='http://www.code.google.com/p/mrbugboto')
+	    profile_url='http://www.mrbugboto.appspont.com')
     myRobot.RegisterHandler(events.BLIP_SUBMITTED, OnBlipSubmitted)
     myRobot.RegisterHandler(events.WAVELET_SELF_ADDED, OnRobotAdded)
     myRobot.Run()
